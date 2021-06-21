d3289d045b9963fab5d6cac883b6a261a90db50b
Sergey S <5818959@gmail.com>
2021-06-21 15:24:09 +0300

    Adds keyboard layout segment for osx.

---
+++ segments/kb_layout.sh
@@ -0,0 +1,22 @@
+# Print the currently used keyboard layout
+# This depends on a specifically developed program which prints the group id of
+# the currently used layout.
+# I developed the simple program myself with some guidance as I was unable to
+# find anything already developed.
+# Some people might suggest:
+# $ setxkbmod -query -v | awk -F "+" '{print $2}'
+# this will only work if you have set up XKB with a single layout which is true
+# for some.
+
+# This script will print the correct layout even if layout is set per window.
+# Exit if platform is not linux as this script is dependant on X11
+
+run_segment() {
+	if ! shell_is_osx; then
+		return 1
+	fi
+
+	cur_layout=`defaults read ~/Library/Preferences/com.apple.HIToolbox.plist AppleSelectedInputSources | egrep -w 'KeyboardLayout\ Name' | sed -E 's/^.+ = \"?([^\"]+)\"?;$/\1/'`
+	[ "$cur_layout" == "ABC" ] && cur_layout="English"
+	echo ${cur_layout:0:2} | awk '{print tolower($0)}'
+}
